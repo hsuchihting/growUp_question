a8056b435508dbd0010cb4f2c6192d5b49abb177
Tim.Hsu <tim.hsu@tpisoftware.com>
2021-08-23 22:52:20 +0800

    addType

--- src/app/pages/add-type/add-type.component.ts
+++ src/app/pages/add-type/add-type.component.ts
@@ -1,5 +1,8 @@
+import { Router } from '@angular/router';
+import { HttpService } from 'src/app/service/http.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { CreateGroupRequest } from 'src/app/model/createGroup-model';
 
 @Component({
   selector: 'app-add-type',
@@ -8,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTypeComponent implements OnInit {
   addTypeForm!: FormGroup;
-  constructor(private _fb: FormBuilder) {}
+  constructor(
+    private _fb: FormBuilder,
+    private _http: HttpService,
+    private _router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createForm();
+  }
 
   createForm() {
     this.addTypeForm = this._fb.group({
@@ -19,7 +28,25 @@ export class AddTypeComponent implements OnInit {
       thirdgroupname: '',
     });
   }
+
   save() {
+    let param = this.addTypeForm.getRawValue();
     alert('新增成功');
+
+    let req: CreateGroupRequest = {
+      id: 'string',
+      secondID: 'string',
+      thirdID: 'string',
+      ...param,
+    };
+    this._http.createGroup(req).subscribe(
+      (res) => {
+        alert('新增成功');
+        this._router.navigate(['/questionFe']);
+      },
+      (err) => {
+        alert('傳送失敗');
+      }
+    );
   }
 }
